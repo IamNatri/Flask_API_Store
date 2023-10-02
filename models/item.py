dd0ea0d3189eedb4d067322b628c950522f08870
Matheus bastos <105763337+IamNatri@users.noreply.github.com>
2023-10-02 15:32:00 -0300

    Change of DB: SqlAlchemy models added

---
+++ models/item.py
@@ -0,0 +1,15 @@
+from db import db
+
+
+class ItemModel(db.Model):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    price = db.Column(db.Float(precision=2), unique=False, nullable=False)
+    store_id = db.Column(
+        db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False
+    )
+    # when have a store id that use store class, populates the store variable with store that have the id associate.
+    store = db.relationship("StoreModel", back_populates="items")
+
